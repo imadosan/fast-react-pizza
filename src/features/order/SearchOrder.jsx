34e707a5ec39fd92db00e3b9c5647d8477c4745a
imadosan <imad.fayad.if@gmail.com>
2024-10-21 12:33:51 +0200

    added tailwind styling for all pages

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery('');
+    setQuery("");
   }
 
   return (
@@ -19,6 +19,7 @@ function SearchOrder() {
           placeholder="Search order #"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          className="foucs:ring-opacity-50 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:w-72 focus:outline-none focus:ring-yellow-500 sm:w-64"
         />
       </div>
     </form>
